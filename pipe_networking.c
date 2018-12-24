423c6c27b2ca7acc7c8958d0d87d847c48d140ed
TimMarder <tmarder@stuy.edu>
2018-12-24 18:32:04 -0500

    cleanup

--- pipe_networking.c
+++ pipe_networking.c
@@ -35,33 +35,6 @@ int server_handshake(int from_client) {
 
     return to_client;
 
-
-    /*
-    char pid[HANDSHAKE_BUFFER_SIZE];
-    char msg[HANDSHAKE_BUFFER_SIZE];
-
-    mkfifo( "wkp", 0666 );
-    printf("WKP created.\n");
-
-    int from_client = open( "wkp", O_RDONLY );
-    printf("Opened client in Read Mode.\n");
-    read( from_client, pid, sizeof( pid ) );
-    printf("Got PID: %s\n", pid);
-
-    remove( "wkp" );
-    printf("Removed Wkp.\n");
-
-    *to_client = open( pid, O_WRONLY );
-    printf("Opened client in Write Mode.\n");
-
-    write( *to_client, ACK, sizeof( ACK ) );
-    printf("Written to Client.\n");
-
-    read( from_client, msg, sizeof( msg ) );
-    printf("Message: %s\n", msg);
-
-    return from_client;*/
-
 }
 
 
@@ -102,31 +75,5 @@ int client_handshake(int *to_server) {
     printf("Finished handshake.\n");
 
     return from_server;
-
-    /*
-    char pid[HANDSHAKE_BUFFER_SIZE];
-    char msg[HANDSHAKE_BUFFER_SIZE];
-
-    sprintf( pid, "%d", getpid() );
-    mkfifo( pid, 0666 );
-    printf("Private FIFO created.\n");
-
-    *to_server = open( pid, O_WRONLY );
-    printf("Opened server in Write Mode.\n");
-    write(*to_server, pid, sizeof( pid ) );
-    printf("Written to server.\n");
-
-    int from_server = open( pid, O_RDONLY );
-    printf("Opened server in Read Mode.\n");
-    read( from_server, msg, sizeof( msg ) );
-    printf("Message: %s\n", msg);
-    remove( pid );
-    printf("Removed PID.\n");
-
-    write(*to_server, ACK, sizeof( ACK ) );
-    printf("Written to server.\n");
-
-    return from_server;
-    */
 
 }
